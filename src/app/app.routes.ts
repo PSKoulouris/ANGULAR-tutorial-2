c07224978f8d0936e8c325f676cb28857f128ac4
PSKoulouris <philippe.koulouris@gmail.com>
2025-06-18 15:53:13 +0200

    com 3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomePage } from './home-page/home-page';
 import { SunRise } from './sun-rise/sun-rise';
 import { WeatherApp } from './weather-app/weather-app';
 import { ForecastPage } from './forecast-page/forecast-page';
+import { CommentsPage } from './comments-page/comments-page';
 
 export const routes: Routes = [
 
     {path:"", component: HomePage},
     {path:"sunrise", component: SunRise},
     {path:"weather", component: WeatherApp},
-    {path:"forecast", component: ForecastPage}
+    {path:"forecast", component: ForecastPage},
+    {path:"comments", component: CommentsPage}
 ];
---
+++ src/app/comments-page/comments-page.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-comments-page',
+  imports: [FormsModule],
+  templateUrl: './comments-page.html',
+  styleUrl: './comments-page.css'
+})
+export class CommentsPage {
+
+  private apiURL = 'https://simple-comments-api.onrender.com/comments'
+
+  commentsList = []
+
+  commentsValue = ""
+
+  // j'install le téléphone chez moi
+  constructor(private http:HttpClient){}
+
+
+  // fonction appel vers API
+  getComments(){
+    this.http.get(this.apiURL).subscribe({
+      next:(data:any) =>{
+        this.commentsList = data;
+      }
+    })
+
+    }
+
+  // Lancer la function dans j'ouvre le composant
+  ngOnInit(){
+    this.getComments();
+  }
+
+  
+  submitForm(){
+    // alert('tu as soumis le formulaire')
+    this.http.post(this.apiURL,{
+      user:'Igor',
+      message:this.commentsValue
+    }).subscribe({
+      next:(res)=>{
+        // ca s'est bien passé, donc on efface l'input et on rafraichie les coms
+        console.log(res);
+        this.commentsValue = '';
+        this.getComments()
+      }
+})
+}
+}
