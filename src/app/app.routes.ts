3c0ed40ee753c4f6ceabd2797de53f64d6a12414
PSKoulouris <philippe.koulouris@gmail.com>
2025-06-18 16:57:32 +0200

    com 3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { SunRise } from './sun-rise/sun-rise';
 import { WeatherApp } from './weather-app/weather-app';
 import { ForecastPage } from './forecast-page/forecast-page';
 import { CommentsPage } from './comments-page/comments-page';
+import { RoomsPage } from './rooms-page/rooms-page';
 
 export const routes: Routes = [
 
@@ -11,5 +12,6 @@ export const routes: Routes = [
     {path:"sunrise", component: SunRise},
     {path:"weather", component: WeatherApp},
     {path:"forecast", component: ForecastPage},
-    {path:"comments", component: CommentsPage}
+    {path:"comments", component: CommentsPage},
+    {path:"rooms", component: RoomsPage}
 ];
